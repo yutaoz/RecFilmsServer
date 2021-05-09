454de7d8bf1c74d2faa726c76d29d0d0b126100d
yutaoz <47333239+yutaoz@users.noreply.github.com>
2021-05-08 23:21:06 -0400

    Added server database add and remove from list functions

---
+++ server.js
@@ -0,0 +1,98 @@
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
+const URI = process.env.DBURI;
+const express = require('express');
+const cors = require('cors');
+
+let app = express();
+
+// when receiving id and movie form server, adds movie to corresponding list in database
+app.get('/add', cors(), async function(req, res) {
+    let id = req.query.id;
+    let movieData = req.query.movie;
+    let movieObj = JSON.parse(movieData);
+
+    const client = new MongoClient(URI);
+
+    try {
+        await client.connect();
+        var db = client.db('recfilms');
+
+        // insert new document if it is new movie list
+        if (await db.collection('lists').countDocuments({id: id}) === 0) {
+            var data = {
+                id: id,
+                movies: [movieObj]
+            }
+            db.collection('lists').insertOne(data, function(err, res) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log('successful addition');
+                }
+            });
+        } else {
+            var movieDoc = await db.collection('lists').findOne(
+                {id: id},
+                {movies: 1}
+            );
+
+            var newList = movieDoc.movies;
+            newList.push(movieObj);
+            await db.collection('lists').updateOne(
+                { id: id },
+                {
+                    $set: {movies: newList}
+                }
+            );
+            console.log(newList);
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+    res.send([id, movieData]);
+});
+
+// removes movie from corresponding list
+app.get('/remove', cors(), async function(req, res) {
+    let id = req.query.id;
+    let movieData = req.query.movie;
+    let movieObj = JSON.parse(movieData);
+
+    const client = new MongoClient(URI);
+
+    try {
+        await client.connect();
+        var db = client.db('recfilms');
+
+        var movieDoc = await db.collection('lists').findOne(
+            {id: id},
+            {movies: 1}
+        );
+
+        let movieList = movieDoc.movies;
+        const index = movieList.indexOf(movieObj);
+        movieList.splice(index, 1);
+        await db.collection('lists').updateOne(
+            { id: id },
+            {
+                $set: {movies: movieList}
+            }
+        );
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+    res.send([id]);
+});
+
+
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log("server listening...");
+});
